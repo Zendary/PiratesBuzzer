25f71d040f11ae29aaf60ae8a60b1d75bcfcac64
Berry1196 <Sebastianberry11@gmail.com>
2023-09-28 10:09:58 +0200

    fixed alot of variables typos

--- src/main.cpp
+++ src/main.cpp
@@ -121,22 +121,9 @@ int durations[] = {
     250, 125, 375, 250, 125, 375,
     125, 125, 125, 125, 125, 500};
 
-#include "Buzzer.h"
+const int sizeOfNotes = sizeof(notes) / sizeof(notes[0]);
 
-// Define your notes and durations arrays here
-int myNotes[] = {
-    NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-    // ... (all the other notes you provided) ...
-    NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4};
-
-int myDurations[] = {
-    125, 125, 250, 125, 125,
-    // ... (all the other durations you provided) ...
-    125, 125, 125, 125, 125, 500};
-
-const int sizeOfNotes = sizeof(myNotes) / sizeof(myNotes[0]);
-
-Buzzer myBuzzer(D7, 1.0, myNotes, myDurations, sizeOfNotes);
+Buzzer myBuzzer(D7, 1.0, notes, durations, sizeOfNotes);
 
 void setup()
 {
@@ -147,5 +134,5 @@ void setup()
 void loop()
 {
   myBuzzer.playNote();
-  delay(500); // Example delay between notes, adjust as needed
+  myBuzzer.update(); // Continuously checks if it's time to play the next note.
 }
